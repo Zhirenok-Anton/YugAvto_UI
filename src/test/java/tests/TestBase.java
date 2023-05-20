b587dd67015d8efc9136bc8a6413d6e4df10b42f
a.zhirenok <a.zhirenok@iz.ru>
2023-05-21 01:57:52 +0300

    back TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import attach.Attach;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,33 +13,56 @@ import java.util.Map;
 
 public class TestBase {
 
-    static WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+    /* @BeforeAll
+     public static void beforeAll() {
 
+         Configuration.baseUrl = "https://demoqa.com";
+         Configuration.browser = "Chrome";
+         Configuration.browserVersion = "100.0";
+         Configuration.browserSize = "1920x1080";
+         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+         DesiredCapabilities capabilities = new DesiredCapabilities();
+         capabilities.setCapability("browserName", "chrome");
+         capabilities.setCapability("browserVersion", "100.0");
+         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                 "enableVNC", true,
+                 "enableVideo", true
+         ));
+
+         Configuration.browserCapabilities = capabilities;
+
+     }*/
     @BeforeAll
     public static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Configuration.baseUrl = webConfig.getBaseUrl();
-        Configuration.browser = webConfig.getBrowser();
-        Configuration.browserVersion = webConfig.getBrowserVersion();
-        Configuration.browserSize = webConfig.getBrowserSize();
-        if (webConfig.getRemoteUrl() != null){
-            Configuration.remote = webConfig.getRemoteUrl();
-        }
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remote_url", "selenoid.autotests.cloud/wd/hub");
+
+
+        Configuration.baseUrl = System.getProperty("base_url", "https://yug-avto.ru" );
+
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1440");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
         Configuration.browserCapabilities = capabilities;
     }
+
 
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
-    
+
+
     @AfterEach
     void addAttachments(){
         Attach.screenshotAs("Last screenshot");
